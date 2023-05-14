080eaa13e4e64d75bbdbb5c5106f42dc7b27b2ad
paulio11 <youngjpj@gmail.com>
2023-05-14 17:07:30 +0100

    Add like views
    Added views for like list and like detail view.

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from .models import Like
+from .serializers import LikeSerializer
+from rest_framework import generics, permissions
+from pokebox.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    A view that provides the list of Likes and allows authenticated users to
+    create new Likes.
+    """
+
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        """
+        Sets the owner of a new Like to the authenticated user making the
+        request.
+        """
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    A view that provides detail of a Like. Only the owner can delete the Like.
+    """
+
+    serializer_class = LikeSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Like.objects.all()
