37063d7c0e2dfe69457f167868098a01fb9a4ca6
paulio11 <youngjpj@gmail.com>
2023-05-14 15:21:44 +0100

    Add comment model
    Added a model for the comments to posts. Includes a owner and post fk, body text, and a created time

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Comment(models.Model):
+    """
+    Represents a Comment made by a User on a Post. Includes a creation
+    timestamp and a text body. Comments are ordered by creation date.
+    """
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    created = models.DateTimeField(auto_now_add=True)
+    body = models.TextField(max_length=400)
+
+    class Meta:
+        ordering = ["-created"]
+
+        def __str__(self):
+            return f"Comment: {self.id}, for post: {self.post}"
