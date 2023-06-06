9e4cce7e95adf3a7aed09e16912823f9b09210d6
paulio11 <youngjpj@gmail.com>
2023-06-06 16:29:57 +0100

    Edit dulplicate like error message

--- likes/serializers.py
+++ likes/serializers.py
@@ -24,4 +24,6 @@ class LikeSerializer(serializers.ModelSerializer):
         try:
             return super().create(validated_data)
         except IntegrityError:
-            raise serializers.ValidationError({"detail": "possible duplicate"})
+            raise serializers.ValidationError(
+                {"detail": "Duplicate like. You have already liked this post."}
+            )
